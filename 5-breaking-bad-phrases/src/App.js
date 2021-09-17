e05f35ac99ab43ccbaf54add0a9ced87b53eee7a
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2021-09-17 01:39:03 -0500

    work on mac

--- 5-breaking-bad-phrases/src/App.js
+++ 5-breaking-bad-phrases/src/App.js
@@ -1,8 +1,40 @@
+import styled from "styled-components";
 
 function App() {
+
+
+  const consultAPI = async () => {
+    
+  }
+
   return (
-    <h1>Breaking Bad</h1>
+    <Container>
+      <Button onClick={consultAPI}>Get Phrase</Button>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+`;
+
+const Button = styled.button`
+  background-color: rgba(255, 255, 255, 0.7);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: black;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
 
 export default App;
