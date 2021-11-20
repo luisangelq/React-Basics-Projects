9eb3980cc41daa1569f7a40892f28eac191fbf6c
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2021-11-20 10:19:31 -0600

    Changing URL API

--- Redux-CRUD/src/config/axiosClient.js
+++ Redux-CRUD/src/config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/luisangelq/React-Basics-Projects/',
+    baseURL: 'http://localhost:4000/',
     headers: {
         'Content-Type': 'application/json'
     }
