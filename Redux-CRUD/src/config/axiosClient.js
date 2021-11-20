0abc10c0d9aa992faa8c2a508d50691d83bbc37b
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2021-11-20 10:18:41 -0600

    Changing api URL to work on host

--- Redux-CRUD/src/config/axiosClient.js
+++ Redux-CRUD/src/config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://my-json-server.typicode.com/luisangelq/React-Basics-Projects/',
     headers: {
         'Content-Type': 'application/json'
     }
