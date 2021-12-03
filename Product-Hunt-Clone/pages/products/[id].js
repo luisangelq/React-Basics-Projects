61ad68012b9a2e69e4d325e373f6a7fa36fbd6bf
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2021-12-02 23:41:39 -0600

    Fixing css view on product page

--- Product-Hunt-Clone/pages/products/[id].js
+++ Product-Hunt-Clone/pages/products/[id].js
@@ -24,36 +24,32 @@ const Product = () => {
     <MainLayout>
       {product ? (
         <Container>
-          <LeftSide>
-            <div className="productHeader">
-              <div>
-                <img src={product.imageURL} alt={product.productName} />
-              </div>
-              <div className="productTitle">
-                <h2>{product.productName}</h2>
-                <p>{product.description}</p>
-              </div>
+          <ProductHeader>
+            <div>
+              <img src={product.imageURL} alt={product.productName} />
             </div>
-
-            <div className="productDescription">
+            <div className="productTitle">
+              <h2>{product.productName}</h2>
               <p>{product.description}</p>
-
-              <div className="createdDate">
-                <span>
-                  featured {moment(product.date).startOf("day").fromNow()}
-                </span>
-              </div>
             </div>
-          </LeftSide>
+          </ProductHeader>
 
-          <RightSide>
-            <div className="btnContaines">
-              <button> get it </button>
-              <button className="votes">
-                &#x25B2; upvote · {product.votes}{" "}
-              </button>
+          <BtnContainer className="btnContaines">
+            <button> get it </button>
+            <button className="votes">
+              &#x25B2; upvote · {product.votes}{" "}
+            </button>
+          </BtnContainer>
+
+          <ProductDescription>
+            <p>{product.description}</p>
+
+            <div className="createdDate">
+              <span>
+                featured {moment(product.date).startOf("day").fromNow()}
+              </span>
             </div>
-          </RightSide>
+          </ProductDescription>
         </Container>
       ) : (
         <Spinner />
@@ -66,73 +62,82 @@ const Container = styled.div`
   width: 90%;
   margin: 5rem auto;
   display: grid;
-  grid-template-columns: 2fr 1fr;
-`;
-const LeftSide = styled.div`
-  margin: 2rem;
-  .productHeader {
+  grid-template-columns: 1fr 1fr;
+
+  @media (max-width: 1000px) {
     display: flex;
-    img {
-      width: 10rem;
-      height: 10rem;
-      border-radius: 0.5rem;
-    }
-
-    .productTitle {
-      margin: 1rem 2rem;
-      color: var(--font-primary-color);
-    }
+    flex-direction: column;
   }
+`;
 
-  .productDescription {
-    margin-top: 5rem;
-    padding: 2rem;
-    border: 1px solid var(--gray);
+const ProductHeader = styled.div`
+  display: flex;
+  img {
+    width: 10rem;
+    height: 10rem;
     border-radius: 0.5rem;
-    background: white;
+  }
 
-    .createdDate {
-      display: flex;
-      justify-content: flex-end;
-      span {
-        margin-top: 5rem;
-        padding: 0.5rem;
-        border: 1px solid var(--gray);
-        border-radius: 0.5rem;
-        font-size: 1rem;
-        color: var(--font-primary-color);
-        text-transform: uppercase;
-      }
+  .productTitle {
+    margin: 1rem 2rem;
+    color: var(--font-primary-color);
+  }
+`;
+
+const BtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 5rem;
+  margin-top: 5rem;
+  gap: 1rem;
+
+  button {
+    outline: none;
+    padding: 1.5rem;
+    min-width: 10rem;
+    color: var(--font-primary-color);
+    font-size: 1.2rem;
+    border: 1px solid var(--gray);
+    background: white;
+    border-radius: 0.5rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    cursor: pointer;
+
+    
+  }
+
+  .votes {
+    min-width: 25rem;
+    background: #f64900;
+    color: white;
+
+    @media (max-width: 480px) {
+      min-width: 20rem;
     }
   }
 `;
 
-const RightSide = styled.div`
-  .btnContaines {
+const ProductDescription = styled.div`
+  margin-top: 5rem;
+  padding: 2rem;
+  border: 1px solid var(--gray);
+  border-radius: 0.5rem;
+  background: white;
+  grid-area: 2 / 1 / 3 / 3;
+
+
+  .createdDate {
     display: flex;
-    justify-content: space-between;
-    margin-top: 5rem;
-    gap: 1rem;
-
-    button {
-      outline: none;
-      padding: 1.5rem;
-      min-width: 10rem;
-      color: var(--font-primary-color);
-      font-size: 1.2rem;
+    justify-content: flex-end;
+    span {
+      margin-top: 5rem;
+      padding: 0.5rem;
       border: 1px solid var(--gray);
-      background: white;
       border-radius: 0.5rem;
+      font-size: 1rem;
+      color: var(--font-primary-color);
       text-transform: uppercase;
-      font-weight: bold;
-      cursor: pointer;
-    }
-
-    .votes {
-        min-width: 25rem;
-      background: #f64900;
-      color: white;
-
     }
   }
 `;
