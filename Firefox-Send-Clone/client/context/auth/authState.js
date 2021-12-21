01d85444efc202ae3af4704d467dbc53df650eec
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2021-12-20 19:57:03 -0600

    .

--- Firefox-Send-Clone/client/context/auth/authState.js
+++ Firefox-Send-Clone/client/context/auth/authState.js
@@ -1,8 +1,19 @@
 import { useReducer } from "react";
 import AuthContext from "./authContext";
+import authReducer from "./authReducer";
 
 const AuthState = ({ children }) => {
 
+    const initialState = {
+        user : null,
+        token: null,
+        isAuthenticated: null,
+        msg : null,
+        
+
+    };
+    
+    const [state, dispatch] = useReducer(authReducer, initialState);
 
     return (
         <AuthContext.Provider
